6faf77da1e2652ee239a353c04e1150973e49703
Alonso de Oliveira <alonsoboj@gmail.com>
2022-03-09 10:21:25 -0300

    WIP: Refatoração de parte da classe de conexão à API do Twitch.

--- API/Configuration/Connection.cs
+++ API/Configuration/Connection.cs
@@ -6,32 +6,35 @@ using System.Threading.Tasks;
 
 namespace API.Configuration
 {
-    public class Connection
+  public class Connection
+  {
+    // Propriedade que representa o client ID da API do Twitch.
+    public string ClientId { get; } = "";
+
+    // Constantes que representam o client secret e grant type da API do Twitch. 
+    const string CLIENT_SECRET = "";
+    const string GRANT_TYPE = "client_credentials";
+
+    public async Task<TokenViewModel> GetToken()
     {
-        public string ClientId { get {return ""; } }
-        private string ClientSecret { get {return ""; } }
-        private string GrantType { get {return "client_credentials";} }
+      var client = new HttpClient();
 
-        public async Task<TokenViewModel> GetToken()
-        {
-            var client = new HttpClient();
+      var values = new Dictionary<string, string>
+      {
+        { "client_id", ClientId },
+        { "client_secret", CLIENT_SECRET },
+        { "grant_type", GRANT_TYPE }
+      };
 
-            var values = new Dictionary<string, string>
-            {
-                { "client_id", ClientId },
-                { "client_secret", ClientSecret },
-                { "grant_type", GrantType }
-            };
+      var content = new FormUrlEncodedContent(values);
 
-            var content = new FormUrlEncodedContent(values);
+      var response = await client.PostAsync("https://id.twitch.tv/oauth2/token", content);
 
-            var response = await client.PostAsync("https://id.twitch.tv/oauth2/token", content);
+      var responseString = await response.Content.ReadAsStringAsync();
 
-            var responseString = await response.Content.ReadAsStringAsync();
+      var result = JsonConvert.DeserializeObject<TokenViewModel>(responseString);
 
-            var result = JsonConvert.DeserializeObject<TokenViewModel>(responseString);
-
-            return result;
-        }
+      return result;
     }
+  }
 }
