77c55f3698722a6e84cd428ecf5ebff56ba664b5
Alonso de Oliveira <alonsoboj@gmail.com>
2022-03-13 20:42:13 -0300

    Criação das entidades.

---
+++ API/Entities/Clip.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace API.Entities
+{
+  public class Clip
+  {
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+
+    [JsonPropertyName("embed_url")]
+    public string EmbedUrl { get; set; }
+
+    [JsonPropertyName("broadcaster_id")]
+    public string BroadcasterId { get; set; }
+
+    [JsonPropertyName("broadcaster_name")]
+    public string BroadcasterName { get; set; }
+
+    [JsonPropertyName("creator_id")]
+    public string CreatorId { get; set; }
+
+    [JsonPropertyName("creator_name")]
+    public string CreatorName { get; set; }
+
+    [JsonPropertyName("video_id")]
+    public string VideoId { get; set; }
+
+    [JsonPropertyName("game_id")]
+    public string GameId { get; set; }
+
+    [JsonPropertyName("language")]
+    public string Language { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("view_count")]
+    public int ViewCount { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("thumbnail_url")]
+    public string ThumbnailUrl { get; set; }
+
+    [JsonPropertyName("duration")]
+    public double Duration { get; set; }
+  }
+}
