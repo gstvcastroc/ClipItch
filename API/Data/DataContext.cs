27f4d322dd7093c3c7411bf121310b92b153962d
Alonso de Oliveira <alonsoboj@gmail.com>
2022-03-13 20:42:01 -0300

    Mapeamento das entidades.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using API.Data.Mappings;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace API.Data
+{
+  public class DataContext : DbContext
+  {
+    public DbSet<Clip> Clips { get; set; }
+    public DbSet<Game> Games { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data Source=Clipitch.db")
+      
+      .LogTo(Console.WriteLine);
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.ApplyConfiguration(new ClipMap());
+      modelBuilder.ApplyConfiguration(new GameMap());
+    }
+  }
+}
