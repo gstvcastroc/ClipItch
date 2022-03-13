48cd44fb4b56bffaf0ed1420253b56ab7b236cff
Alonso de Oliveira <alonsoboj@gmail.com>
2022-03-13 20:43:03 -0300

    Adição dos workers.

---
+++ API/Workers/ClipsWorker.cs
@@ -0,0 +1,24 @@
+﻿using API.Interfaces;
+using Coravel.Invocable;
+using System.Threading.Tasks;
+
+namespace API.Workers
+{
+  public class ClipsWorker : IInvocable
+  {
+
+    private readonly IClipsService _clipsService;
+
+    public ClipsWorker(IClipsService clipsService)
+    {
+      _clipsService = clipsService;
+    }
+
+    public async Task Invoke()
+    {
+      var clipsList = await _clipsService.GetClipsFromTwitchAsync();
+
+      await _clipsService.AddClipsToDatabaseAsync(clipsList);
+    }
+  }
+}
