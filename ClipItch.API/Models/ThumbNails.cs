4bd0c2726d1107720b4115bf091aea37a66340dc
Alonso de Oliveira <alonsoboj@gmail.com>
2022-03-13 20:39:47 -0300

    Reorganização da estrutura do projeto.

--- ClipItch.API/Models/ThumbNails.cs
+++
@@ -1,9 +0,0 @@
-namespace ClipItch.API.Models
-{
-    public class ThumbNails
-    {
-        public string Medium { get; set; }
-        public string Small { get; set; }
-        public string Tiny { get; set; }
-    }
-}
