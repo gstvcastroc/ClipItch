2a16e08a6c2dfcdc8c8893cbdab5118b283f988f
Alonso de Oliveira <alonsoboj@gmail.com>
2022-03-18 18:24:45 -0300

    Adição de nova interface para a funcionalidade de busca.

--- API/Interfaces/ISearchService.cs
+++ API/Interfaces/ISearchService.cs
@@ -1,7 +1,11 @@
-﻿namespace API.Interfaces
+﻿using API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
 {
   public interface ISearchService
   {
-
+    Task<string> SearchClipsAsync(string input);
   }
 }
